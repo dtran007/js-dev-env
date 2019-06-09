be0c5320842ed805c9986aa177e7e471db93206b
Danny Tran <dtran007@protonmail.com>
2019-06-09 14:04:58 -0700

    add transpile with babel

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,5 @@
-var chalk = require('chalk');
+//var chalk = require('chalk'); //ES5 Pattern
+//Testing babel ES6 transpile
+import chalk from 'chalk'; //change node to babel-node in package.json
 
 console.log(chalk.green('Starting app in dev mode...'));
